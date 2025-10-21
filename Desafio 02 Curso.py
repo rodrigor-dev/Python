6fef382d36dd4128de206ab4a688f6f41d9deaaa
Rodrigo R. Dev <rodrigorodrigues3664@gmail.com>
2025-10-21 15:32:17 -0300

    Update Desafio 02 Curso.py

--- Desafio 02 Curso.py
+++ Desafio 02 Curso.py
@@ -1,4 +1,5 @@
 dia = input('Dia = ')
 mes = input('Mês = ')
 ano = input('Ano = ')
-print('Você nasceu no dia ', dia +' de '+ mes +' de ', ano +'.'+ ' Correto?')
+print('Você nasceu no dia ', dia ,' de ', mes ,' de ', ano ,'.', ' Correto?')
+
