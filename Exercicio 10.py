8a09d28e6c22117f097a989d82bdb2f82224fbda
Rodrigo R. Dev <rodrigorodrigues3664@gmail.com>
2025-11-04 16:51:13 -0300

    Add files via upload

---
+++ Exercicio 10.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um numero: '))
+x1 = n1 * 1
+x2 = n1 * 2
+x3 = n1 * 3
+x4 = n1 * 4
+x5 = n1 * 5
+x6 = n1 * 6
+x7 = n1 * 7
+x8 = n1 * 8
+x9 = n1 * 9
+x10 = n1 * 10
+print(f'A tabuada de {n1} É :\n {n1} x 1 = {x1}\n {n1} x 2 = {x2}\n {n1} x 3 = {x3}\n {n1} x 4 = {x4}\n {n1} x 5 = {x5}\n {n1} x 6 = {x6}\n {n1} x 7 = {x7}\n {n1} x 8 = {x8}\n {n1} x 9 = {x9}\n {n1} x 10 = {x10}')
