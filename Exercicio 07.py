dbcdbb587dfba1b867ebb2c7e1a3f030b2a3b160
Rodrigo R. Dev <rodrigorodrigues3664@gmail.com>
2025-11-04 16:24:16 -0300

    Add files via upload

---
+++ Exercicio 07.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+do = n1 * 2
+tri = n1 * 3
+r4 = int( n1 ** (1/2))
+print(f'O numero que você digitou foi {n1} o dobro dele é {do} o triplo é {tri} e a raiz quadrada dele é {r4}!')
