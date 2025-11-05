8c6b5b2d2296be7f32a7cc622689124d91a1752e
Rodrigo R. Dev <rodrigorodrigues3664@gmail.com>
2025-11-05 14:51:40 -0300

    Update Exercicio 08.py

--- Exercicio 08.py
+++ Exercicio 08.py
@@ -2,4 +2,4 @@ n1 = float(input('Digite a primeira nota do aluno: '))
 n2 = float(input('Digite a segunda nota do aluno: '))
 m = (n1 + n2)/2
 
-print(f'A media entre {n1} e {n2} é igual a {m}!')
+print(f'A media entre {n1} e {n2} é igual a {m:.1f}!')
