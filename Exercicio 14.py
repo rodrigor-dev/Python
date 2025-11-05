081738cd22a91c8d0312ef5813ac2837e062b4f9
Rodrigo R. Dev <rodrigorodrigues3664@gmail.com>
2025-11-05 17:15:15 -0300

    Add files via upload

---
+++ Exercicio 14.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual o salário do funcionário: R$'))
+aumento = salario + (salario * 15/100)
+print(f'Um funcionário que ganhava R${salario:.2f}, com 15% de Aumento passa a receber:R${aumento:.2f}')
